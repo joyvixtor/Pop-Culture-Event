275b80520ae0b067360b00336b9caa09d3de0445
João Victor de Melo Bezerra <joaovicmelobez@gmail.com>
2025-03-09 22:13:47 -0300

    feat: adding Participantes table and its specializations

---
+++ creatingTables.sql
@@ -0,0 +1,44 @@
+-- Tabela Endereco
+CREATE TABLE Endereco (
+    cep VARCHAR(9) PRIMARY KEY,
+    estado VARCHAR2(50),
+    cidade VARCHAR2(50),
+    bairro VARCHAR2(50),
+    rua VARCHAR2(50),
+    numero VARCHAR2(10)
+);
+
+/* 
+    Tabela Participante 
+    ESPECIALIZACOES: Ouvinte, Palestrante, Organizador
+*/
+CREATE TABLE Participante (
+    cpf VARCHAR2(11) PRIMARY KEY,
+    nome VARCHAR2(100) NOT NULL,
+    email VARCHAR2(50),
+    telefone VARCHAR2(15),
+    cep VARCHAR2(9),
+    CONSTRAINT fk_participante_cep FOREIGN KEY (cep) REFERENCES Endereco(cep),
+    CONSTRAINT chk_cep CHECK (LENGTH(cep) = 9)
+);
+
+CREATE TABLE Ouvinte (
+    cpf_participante VARCHAR2(11) PRIMARY KEY,
+    numero_do_ingresso VARCHAR(15),
+    CONSTRAINT fk_ouvinte_participante FOREIGN KEY (cpf_participante) REFERENCES Participante(cpf)
+);
+
+CREATE TABLE Palestrante (
+    cpf_participante VARCHAR2(11) PRIMARY KEY,
+    empresa_vinculo VARCHAR2(100),
+    CONSTRAINT fk_palestrante_participante FOREIGN KEY (cpf_participante) REFERENCES Participante(cpf)
+);
+
+CREATE TABLE Organizador (
+    cpf_participante VARCHAR2(11) PRIMARY KEY,
+    cargo VARCHAR2(50),
+    departamento VARCHAR2(50),
+    cpf_supervisor VARCHAR2(11),
+    CONSTRAINT fk_organizador_participante FOREIGN KEY (cpf_participante) REFERENCES Participante(cpf),
+    CONSTRAINT fk_organizador_supervisor FOREIGN KEY (cpf_supervisor) REFERENCES Organizador(cpf_participante)
+);
